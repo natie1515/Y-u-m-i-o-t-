38338399afb8254d76c2716238c720e8ee877c21
natie1515 <frejulian57@gmail.com>
2025-11-23 13:03:40 -0400

    Update WhatsApp channel ID in settings.js

--- settings.js
+++ settings.js
@@ -47,7 +47,7 @@ global.channel = "https://whatsapp.com/channel/0029VbAc6cS002TEZ4r5261E"
 global.github = "https://github.com/The-King-Destroy/YukiBot-MD"
 global.gmail = "thekingdestroy507@gmail.com"
 global.ch = {
-ch1: "120363401646371525@newsletter"
+ch1: "120363188537623366@newsletter"
 }
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
