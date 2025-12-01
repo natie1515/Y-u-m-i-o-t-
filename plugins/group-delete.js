dfc66f6fd2d10162eb3a118083e2de0f06bdb87f
natie1515 <frejulian57@gmail.com>
2025-12-01 00:05:55 -0400

    Update group-delete.js

--- plugins/group-delete.js
+++ plugins/group-delete.js
@@ -1,21 +1,27 @@
-let handler = async (m, { conn, command }) => {
-if (!m.quoted) {
-return conn.reply(m.chat, `❀ Por favor, cita el mensaje que deseas eliminar.`, m)
+let handler = async (m, { conn }) => {
+  if (!m.quoted) {
+    return conn.reply(m.chat, `❀ Por favor, cita el mensaje que deseas eliminar.`, m)
+  }
+
+  try {
+    return conn.sendMessage(m.chat, {
+      delete: {
+        remoteJid: m.chat,
+        fromMe: false,
+        id: m.quoted.key.id,
+        participant: m.quoted.key.participant
+      }
+    })
+  } catch {
+    return conn.sendMessage(m.chat, { delete: m.quoted.key })
+  }
 }
-try {
-let participant = m.message.extendedTextMessage.contextInfo.participant
-let stanzaId = m.message.extendedTextMessage.contextInfo.stanzaId
-return conn.sendMessage(m.chat, {
-delete: { remoteJid: m.chat, fromMe: false, id: stanzaId, participant: participant }})
-} catch {
-return conn.sendMessage(m.chat, { delete: m.quoted.key })
-}}
 
 handler.help = ['delete']
 handler.tags = ['grupo']
 handler.command = ['del', 'delete']
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
+handler.group = true       // Solo en grupos
+handler.admin = true       // Solo admins pueden usarlo
+handler.botAdmin = true    // El bot debe ser admin
 
 export default handler
